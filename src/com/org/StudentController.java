c8df26063f9316d2081c9ef24229e80f49b7cef3
vishnu <vishnu>
2016-07-27 18:36:22 +0530

    first commit

---
+++ src/com/org/StudentController.java
@@ -0,0 +1,27 @@
+package com.org;
+
+import org.springframework.stereotype.Controller; 
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.ui.ModelMap;
+
+@Controller
+public class StudentController {
+
+   @RequestMapping(value = "/student", method = RequestMethod.GET)
+   public ModelAndView student() {
+      return new ModelAndView("student", "command", new Student());
+   }
+   
+   @RequestMapping(value = "/result1", method = RequestMethod.POST)
+   public String addStudent(@ModelAttribute("SpringFormMVC")Student student, //@ModelAttribute as method argument.It is called data binding in spring mvc.
+   ModelMap model) {
+      model.addAttribute("name", student.getName());
+      model.addAttribute("age", student.getAge());
+      model.addAttribute("id", student.getId());
+      
+      return "result";
+   }
+}
